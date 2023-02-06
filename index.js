d1b4df0584914257552111ce124b6a08207d26cf
Fernando Marini <fer.ms114@gmail.com>
2023-02-06 10:39:22 -0300

    reto1

---
+++ index.js
@@ -0,0 +1,29 @@
+
+function suma(op1,op2=0){
+    result = op1 + op2;
+    return result
+};
+
+function resta(op1,op2=0){
+    result = op1 - op2;
+    return result
+};
+
+function producto(op1,op2=0){
+    result = op1 * op2;
+    return result
+};
+
+function division(op1,op2=0){
+    result = op1 / op2;
+    return result
+};
+
+function cuadradoNum ( n ){
+    result = n * n;
+    return result
+};
+
+
+
+module.exports = {suma,resta,producto,division,cuadradoNum}
---
+++ run.js
@@ -0,0 +1,7 @@
+let myLib = require("./index");
+
+console.log(myLib.suma(1,2));
+console.log(myLib.producto(1,2));
+console.log(myLib.resta(9,2));
+console.log(myLib.division(8,2));
+console.log(myLib.cuadradoNum(5));
